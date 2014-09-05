2a745351b460c297fbedda4f104464cd9493d01b
badlis <shivakumar.badli@chep.com>
2014-09-05 10:55:45 +0100

    commit

--- scripts/siebel/custom/theme.js
+++ scripts/siebel/custom/theme.js
@@ -20,7 +20,7 @@ SiebelApp.ThemeManager.addResource(
     }
 );
 */
-/* Shiv comments****/
+/* Shiv comments****More stars/
 /* Start Defect:200 CHAMARS 05 Sep 2014 */
 
 SiebelApp.ThemeManager.addResource(
